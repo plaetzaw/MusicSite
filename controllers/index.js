103f9b56e1820ecc78d96ad2ecfdf403a1685adb
plaetzaw <plaetzaw@gmail.com>
2020-03-02 15:17:38 -0600

    Still settting up site

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+let data = require("../data/data.json");
+
 router.get("/", (req, res) => {
-  res.render("index");
+  let pagePhotos = [];
+  data.speakers.forEach(speakerObj => {
+    pagePhotos = pagePhotos.concat(speakerObj.artwork);
+  });
+  console.log(pagePhotos);
+  res.render("index", { artwork: pagePhotos });
 });
 
 module.exports = router;
--- controllers/speakers.js
+++ controllers/speakers.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const router = express.Router();
+let data = require("../data/data.json");
 
 router.get("/speakers", (req, res) => {
+  //all speakers
+  let pagePhotos = [];
+  data.speakers.forEach(speakerObj => {
+    pagePhotos = pagePhotos.concat(speakerObj.artwork);
+  });
+  res.render("speakers", {
+    pageTitle: "Speaker List",
+    artistName: "",
+    artwork: pagePhotos,
+    allSpeakers: data.speakers,
+    pageID: "speakerList"
+  });
+});
+
+router.get("/speakers/:speakerid", (req, res) => {
   res.render("speakers");
 });
 
